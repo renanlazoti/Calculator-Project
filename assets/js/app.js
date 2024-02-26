a6071d1c10fcc8d1a09b7e93147f91262103d445
Renan Lazoti <renanlazoti@gmail.com>
2024-02-25 22:30:32 -0300

    feat: create digit event and clear function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,56 @@
+let value1 = document.getElementById("first-factor");
+let value2 = document.getElementById("second-factor");
+let operationResult = document.getElementById("result");
+let numberDigits = document.querySelectorAll('.digits');
+let focusedInput = "";
+
+focusedInput = value1.focus();
+
+numberDigits.forEach(function(button) {
+    button.addEventListener('click', function() {       
+        if (focusedInput) {
+            focusedInput.value += button.value;
+
+            focusedInput.focus();
+        }
+    });
+});
+
+value1.addEventListener('focus', function() {
+    focusedInput = value1;
+});
+
+value2.addEventListener('focus', function() {
+    focusedInput = value2;
+});
+
 function Calculate() {
-    var value1 = document.getElementById("first-factor").value;
-    var value2 = document.getElementById("second-factor").value;
-    var operation = document.getElementById("operation").value;
-    var output;
+    let operation = document.getElementById("operation").value;
+    let output;
     
     switch(operation){
         case "+":
-            output = Number(value1) + Number(value2);
+            output = Number(value1.value) + Number(value2.value);
             break;
         case "-":
-            output = Number(value1) - Number(value2);
+            output = Number(value1.value) - Number(value2.value);
             break;
         case "*":
-            output = Number(value1) * Number(value2);
+            output = Number(value1.value) * Number(value2.value);
             break
         case "/":
-            output = Number(value1) / Number(value2);
+            output = Number(value1.value) / Number(value2.value);
             break;
         default: 
             break;
     }
 
-    document.getElementById("result").value = output;
-    
+   operationResult.value = output;
+}
+
+function Clear() {
+    document.getElementById("first-factor").value = null;
+    document.getElementById("second-factor").value = null;
+    document.getElementById("result").value = null;
+    value1.focus();
 }
